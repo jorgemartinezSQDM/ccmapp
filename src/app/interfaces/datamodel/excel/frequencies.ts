8d58434b5e666f3a7091409c3131f2dd3cd36d96
Jhair Herrera <jhaircausil@sqdm.com>
2022-07-05 08:44:09 -0500

    Correciones de palabras

--- src/app/interfaces/datamodel/excel/frequencies.ts
+++ src/app/interfaces/datamodel/excel/frequencies.ts
@@ -3,8 +3,8 @@ export interface ID_xFrequencies {
   "Id del cliente": any;
   "Nombre del cliente": any;
   "Apellidos del cliente": any;
-  "Id de la camapaña": any;
-  "Nombre de la camapaña": any;
+  "Id de la campaña": any;
+  "Nombre de la campaña": any;
   "Toques del día": any;
   "Fecha de creación": any;
   "Fecha de modificación": any;
